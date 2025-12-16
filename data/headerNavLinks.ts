14348e590a90fdabfb640fb8ef7626f3932a687e
xirothedev <lethanhtrung.trungle@gmail.com>
2025-12-17 01:03:22 +0700

    Remove blog page implementation and corresponding navigation link from header. This change simplifies the site structure by eliminating the blog section.

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,6 @@
 const headerNavLinks = [
 	{ href: "/", title: "Home" },
 	{ href: "https://xirothedev.site", title: "Portfolio", target: "_blank" },
-	{ href: "/blog", title: "Blog" },
 	{ href: "/tags", title: "Tags" },
 	{ href: "/about", title: "About" },
 ];
